9bd4335bc92c40e69c6653432db34d04ad730d68
ejthomas <ejthomaswork@gmail.com>
2023-02-06 10:15:01 -0600

    Comment Editing Commit

--- MyFirstProject/src/com/tek/example1/FirstClass.java
+++ MyFirstProject/src/com/tek/example1/FirstClass.java
@@ -1,5 +1,12 @@
 package com.tek.example1;
 
+/*
+ * Messing around with comment use.
+ * It helps to explain, get ideas across, or convey what your code is about.
+ */
+
+// Oh the many ways you can utilize comments though!
+
 public class FirstClass {
 
 	public static void main( String[] arg) {
